d3eb44e55f989eb72307817d865363ddf973a29a
dust-machine-dds <tk4934939@gmail.com>
2022-08-27 12:59:07 +0500

    Integrated Contact and footer section

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+    
+    var typed = new Typed(".typing",{
+        strings:["Web Developer","Graphic Designer","App Developer","Teacher","Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+    var typed = new Typed(".typing2",{
+        strings:["Web Developer","Graphic Designer","App Developer","Teacher","Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    //scroll function
     $(window).scroll(function(){
         if(scrollY>20){
             $('.navbar').addClass('sticky')
@@ -6,10 +21,18 @@ $(document).ready(function(){
         else{
             $('.navbar').removeClass('sticky')
         }
+
+        if(this.scrollY>500){
+            $('.scroll-up-btn').addClass('show')  
+        }
+        else{
+            $('.scroll-up-btn').removeClass('show')  
+        }
     });
     //togglemenubutton/navbar
     $('.menubtn').click(function(){
         $('.navbar .menu').toggleClass('active');
         $('.menubtn i').toggleClass('active');
     })
+
 })
